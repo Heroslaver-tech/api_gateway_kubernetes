39005e758f14a1b7c1d0dfb328805d873117879e
heroslaver <sebas152000@hotmail.com>
2024-04-17 13:33:21 -0500

    fix errors

--- servicio-4/servicio-4.js
+++ servicio-4/servicio-4.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-app.get('/servicio-4', (req, res) => {
+app.get('/service-4', (req, res) => {
   res.json({ mensaje: "Respuesta desde Servicio Prueba, soy Daniel y me gustan los videojuegos" });
 });
 
